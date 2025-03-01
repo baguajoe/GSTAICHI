86e2d1d7ee587c0485cf484c2b8d823953879b29
baguajoe <baguajoe@gmail.com>
2025-03-01 17:34:52 +0000

    added logout and it works

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask import request, jsonify, url_for, redirect, Response
-from flask_admin import Admin, AdminIndexView, expose
+from flask_admin import Admin, AdminIndexView, expose, BaseView
 from .models import db, User, Classes, Book, Video, Article
 from flask_admin.contrib.sqla import ModelView
 
@@ -13,6 +13,14 @@ from flask_admin.contrib.sqla import ModelView
 #         return auth.username == os.environ.get("ADMIN_USER", "admin") and \
 #                auth.password == os.environ.get("ADMIN_PASS", "your-secure-password") 
         
+class LogoutView(BaseView):
+    @expose('/')
+    def index(self):
+        return Response(
+            "Logged out successfully. Close this window and reopen the admin page to log in again.",
+            401,
+            {"WWW-Authenticate": "Basic realm='Login Required'"}
+        )
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
@@ -27,5 +35,8 @@ def setup_admin(app):
     # admin.add_view(ModelView(Video, db.session))
     admin.add_view(ModelView(Article, db.session))
 
+
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
+
+    admin.add_view(LogoutView(name='Logout', endpoint='logout'))
